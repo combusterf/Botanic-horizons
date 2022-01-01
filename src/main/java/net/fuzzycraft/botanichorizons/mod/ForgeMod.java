ba17260884e07d8be5958537119f722ab2d59086
Johann Bernhardt <johann.bernhardt@tum.de>
2022-01-01 22:53:55 +0100

    Migrated to unified build script

--- src/main/java/net/fuzzycraft/botanichorizons/mod/ForgeMod.java
+++ src/main/java/net/fuzzycraft/botanichorizons/mod/ForgeMod.java
@@ -9,9 +9,8 @@ import net.fuzzycraft.botanichorizons.patches.*;
 public class ForgeMod {
     public static final String MOD_ID = "botanichorizons";
     public static final String MOD_NAME = MOD_ID;
-    public static final String BUILD = "GRADLE:BUILD";
-    public static final String VERSION = "GRADLE:VERSION-" + BUILD;
-    public static final String DEPENDENCIES = "required-after:Baubles;required-after:Thaumcraft;required-after:Botania;required-after:gregtech;after:witchery;after:BiomesOPlenty";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
+    public static final String DEPENDENCIES = "after:Baubles;required-after:Thaumcraft;required-after:Botania;required-after:gregtech;after:witchery;after:BiomesOPlenty";
 
     @Mod.Instance(MOD_ID)
     public static ForgeMod instance;
