040d7dd597aec8ea3427998bbfebb513d8f2ac0e
Martin Robertz <dream-master@gmx.net>
2022-08-19 16:45:55 +0200

    add screws

--- src/main/java/net/fuzzycraft/botanichorizons/patches/CraftingPatches.java
+++ src/main/java/net/fuzzycraft/botanichorizons/patches/CraftingPatches.java
@@ -58,12 +58,13 @@ public class CraftingPatches {
         // Petal Apothecary Recipes
         for(int i = 0; i < 16; i++)
             addOreDictRecipe(new ItemStack(ModBlocks.altar),
-                    "SPS", " U ", "CTC",
+                    "SPS", "XUX", "CTC",
                     'S', "plateSteel",
                     'P', OreDict.FLOWER_INGREDIENT[i],
                     'U', new ItemStack(Items.cauldron),
                     'C', "stone",
-                    'T', "blockSilver");
+                    'T', "blockSilver",
+                    'X', "screwSteel");
 
         ModCraftingRecipes.recipesApothecary = BotaniaAPI.getLatestAddedRecipes(16);
 
