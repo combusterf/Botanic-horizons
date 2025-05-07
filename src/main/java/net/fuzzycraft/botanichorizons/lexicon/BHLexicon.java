36c59a811192bd9379ff914a5abebb1ac0ee07fc
Marcel Sondaar <noreply@combuster.nl>
2025-05-07 21:03:00 +0200

    feature: Improved lexicon entries and added icons

--- src/main/java/net/fuzzycraft/botanichorizons/lexicon/BHLexicon.java
+++ src/main/java/net/fuzzycraft/botanichorizons/lexicon/BHLexicon.java
@@ -2,11 +2,14 @@ package net.fuzzycraft.botanichorizons.lexicon;
 
 import net.fuzzycraft.botanichorizons.addons.BHBlocks;
 import net.fuzzycraft.botanichorizons.addons.Multiblocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.lexicon.LexiconCategory;
 import vazkii.botania.api.lexicon.LexiconEntry;
-import vazkii.botania.common.lexicon.BLexiconEntry;
+import vazkii.botania.common.lexicon.page.PageGuide;
 import vazkii.botania.common.lexicon.page.PageMultiblock;
+import vazkii.botania.common.lexicon.page.PageText;
 
 public final class BHLexicon {
     private BHLexicon() {}
@@ -20,11 +23,18 @@ public final class BHLexicon {
     }
 
     public static void init() {
+        LexiconEntry titlePage = new BHLexiconEntry("multiblockInfo", multiblockCategory);
+        titlePage.addPage(new PageText("botanichorizons.lexicon.text.multiblockInfo.1"));
+        titlePage.setPriority();
+        titlePage.setIcon(new ItemStack(Items.writable_book));
+
         LexiconEntry gatewayEntry = new BHLexiconEntry("automatedAlfPortal", multiblockCategory);
+        gatewayEntry.addPage(new PageText("botanichorizons.lexicon.text.automatedAlfPortal.1"));
         gatewayEntry.addPage(new PageMultiblock("botanichorizons.lexicon.preview.automatedAlfPortal",
                 Multiblocks.alfPortal.lexiconMultiblock(
                         0, 1, -2,
                         BHBlocks.autoPortal, 0
                 )));
+        gatewayEntry.setIcon(new ItemStack(BHBlocks.autoPortal));
     }
 }
